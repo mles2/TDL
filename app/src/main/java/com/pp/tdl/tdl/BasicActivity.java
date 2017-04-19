b501aec4e84cb19accfe0e45cedbb0e2010cdcfd
mles <michalles3@gmail.com>
2017-04-19 23:40:36 +0200

    added example code for display of list implementation

--- app/src/main/java/com/pp/tdl/tdl/BasicActivity.java
+++ app/src/main/java/com/pp/tdl/tdl/BasicActivity.java
@@ -1,5 +1,6 @@
 package com.pp.tdl.tdl;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class BasicActivity extends AppCompatActivity {
 
@@ -22,6 +24,7 @@ public class BasicActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(new Intent(BasicActivity.this,ListViewLoader.class));
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
